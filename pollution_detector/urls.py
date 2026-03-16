794227be81040ee2bc0d7dc243ba1254884cea94
AnishDaswani <akhem2010@gmail.com>
2026-03-16 12:47:02 -0400

    Optimizing and cleaning node tree so code is easier to understand

--- pollution_detector/urls.py
+++ pollution_detector/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from ml_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ml_app.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.index, name="index"),
+    path("graphs/", views.graphs, name="graphs"),
+    path("predict/", views.predict, name="predict"),
+    path("predict/upload/", views.predict_upload, name="predict_upload"),
+    path("train/", views.train_page, name="train"),
+    path("train/start/", views.start_training, name="start_training"),
+    path("train/progress/", views.get_training_progress, name="training_progress"),
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
