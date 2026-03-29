3b25d31b71e7e547f1eea14054e6456c0dc64086
AnishDaswani <akhem2010@gmail.com>
2026-03-29 13:36:57 -0400

    didn't even take that long, just updated url, removed unnecesary readmes

--- web/pollution_detector/settings.py
+++ web/pollution_detector/settings.py
@@ -21,7 +21,7 @@ ROOT_URLCONF = 'pollution_detector.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [],
